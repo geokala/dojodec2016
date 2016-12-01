798f672bc73b56c836fa0b8ab9eb15082f8bc1c5
Matthew Power <matthew@mthpower.uk>
2016-12-01 20:46:03 +0000

    not a DS_Store this time

--- trump.py
+++ trump.py
@@ -1,3 +1,17 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageFont, ImageDraw
 
-im = Image.open("boat.jpg")
+import tweets
+
+tweets = tweets.get_tweets()
+
+for i, tweet in enumerate(tweets):
+
+    base = Image.open("boat.jpg").convert('RGBA')
+    fnt = ImageFont.truetype('Impact.ttf', 30)
+    txt = Image.new('RGBA', base.size, (255, 255, 255, 0))
+
+    d = ImageDraw.Draw(txt)
+    d.text((10, 60), tweet, font=fnt, fill=(255, 255, 255, 255))
+
+    out = Image.alpha_composite(base, txt)
+    out.save('output/{}'.format(i), 'JPEG')
