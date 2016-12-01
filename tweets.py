47c8aae0e0e38f93a3d484171f846365cbb44538
Jake Gillespie <jake.gillespie@onefinestay.com>
2016-12-01 20:38:45 +0000

    Functions

--- tweets.py
+++ tweets.py
@@ -5,10 +5,14 @@ import bs4
 URL = 'https://twitter.com/realDonaldTrump'
 
 
-response = requests.get(url=URL)
-data = bs4.BeautifulSoup(response.text, 'html.parser')
-tweets_html = data.find_all('p', 'js-tweet-text')
-tweets = [tweet.findAll(text=True)[0] for tweet in tweets_html]
+def get_tweets():
+    response = requests.get(url=URL)
+    data = bs4.BeautifulSoup(response.text, 'html.parser')
+    tweets_html = data.find_all('p', 'js-tweet-text')
+    tweets = [tweet.findAll(text=True)[0] for tweet in tweets_html]
 
+    return tweets
+
+tweets = get_tweets()
 print(tweets)
 
